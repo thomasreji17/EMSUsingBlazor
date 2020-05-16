0c709bff8e93f7b83aa9c4219b7eb6f2d2f473f2
thomasreji17@gmail.com <thomasreji17@gmail.com>
2020-05-16 19:57:42 +0530

    Initaial Version

---
+++ EmployeeManagementSystem/EMS.Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EMS.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+        public Gender Gender { get; set; }
+
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Pages/EmployeeList/EmployeeListBase.cs
@@ -0,0 +1,72 @@
+﻿using EMS.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Pages.EmployeeList
+{
+    public class EmployeeListBase:ComponentBase
+    {
+        protected IEnumerable<Employee> employees { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            LoadEmployees();
+            return base.OnInitializedAsync();
+        }
+        private void LoadEmployees()
+        {
+            Employee e1 = new Employee()
+            {
+                Name = "Rose",
+                Age = 25,
+                Email = "Rose@hobbits.com",
+                Gender = Gender.Female,
+                Id = 1,
+                ImageUrl = "images/e1.png"
+            };
+
+            Employee e2 = new Employee()
+            {
+                Name = "Jack",
+                Age = 28,
+                Email = "Jack@hobbits.com",
+                Gender = Gender.Male,
+                Id = 2,
+                ImageUrl = "images/e3.jpg"
+            };
+
+            Employee e3 = new Employee()
+            {
+                Name = "Tony",
+                Age = 30,
+                Email = "Tony@hobbits.com",
+                Gender = Gender.Male,
+                Id = 3,
+                ImageUrl = "images/e3.jpg"
+            };
+
+            Employee e4 = new Employee()
+            {
+                Name = "Steve",
+                Age = 50,
+                Email = "Steve@hobbits.com",
+                Gender = Gender.Male,
+                Id = 4,
+                ImageUrl = "images/e3.jpg"
+            };
+
+            Employee e5 = new Employee()
+            {
+                Name = "Natasha",
+                Age = 32,
+                Email = "Natasha@hobbits.com",
+                Gender = Gender.Female,
+                Id = 5,
+                ImageUrl = "images/e4.png"
+            };
+            this.employees = new List<Employee>() { e1, e2, e3, e4, e5 };
+        }
+    }
+}
